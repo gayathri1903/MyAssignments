7c1baa074abf373e139d238b8f4153f71f88a8fd
gayathri1903 <gayathrikalidasan1903@gmail.com>
2025-08-11 03:02:12 -0700

    Add files via upload

---
+++ addnumber.java
@@ -0,0 +1,15 @@
+
+
+public class addnumber {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("addition value"+(a+b));
+		System.out.println("subtraction"+(a-b));
+		System.out.println("multiplication value"+(a*b));
+		System.out.println("divided value"+(a/b));
+		
+	}
+
+}
+
